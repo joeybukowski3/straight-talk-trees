b315ffe0a41cb9c92307f54b8a9641866074c3d1
joeybukowski3 <joeybukowski3@gmail.com>
2026-07-24 12:25:34 -0400

    Refine brand mark to recognizable pine tree

--- src/components/site/TreeMark.tsx
+++ src/components/site/TreeMark.tsx
@@ -1,5 +1,5 @@
 export const TREE_MARK_PATH =
-  "M16 2c4 3 7 6 7 10a7 7 0 0 1-5 6.7V22h3a1 1 0 0 1 0 2h-3v5a1 1 0 0 1-2 0v-5h-3a1 1 0 0 1 0-2h3v-3.3A7 7 0 0 1 9 12c0-4 3-7 7-10Z";
+  "M16 2 23 11h-4l6 7h-5l6 7h-8v5h-4v-5H6l6-7H7l6-7H9l7-9Z";
 
 export function TreeMark({ className }: { className?: string }) {
   return (
